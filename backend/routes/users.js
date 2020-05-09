d9f2f73de839a3a61967b97fd98dafe6f1a1b70d
Randy Chan <randychan997@gmail.com>
2020-05-08 21:20:17 -0500

    Step 3 completed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var offers = require('../offers.json')
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render(res.json(offers));
 });
 
 module.exports = router;
